227af46f6cb5be4c7b2a79f997f0dbb95d995a4c
gyu98-mun <ansrb537@gmail.com>
2026-02-11 10:11:26 +0900

    첫 커밋 push

---
+++ JavaCLI/Article.java
@@ -0,0 +1,15 @@
+package JavaCLI;
+
+public class Article {
+    int id;
+    String title;
+    String content;
+    String regDate;
+    
+    public Article(int id, String title, String content, String regDate) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.regDate = regDate;
+    }
+}
---
+++ JavaCLI/Rq.java
@@ -0,0 +1,5 @@
+package JavaCLI;
+
+public class Rq {
+
+}
