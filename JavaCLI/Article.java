f389ae63afdcb72ee6ebdf20fa2665e7a8264fec
gyu98-mun <ansrb537@gmail.com>
2026-02-12 17:11:48 +0900

    11234

--- JavaCLI/Article.java
+++ JavaCLI/Article.java
@@ -5,6 +5,7 @@ public class Article {
     String title;
     String content;
     String regDate;
+    String articleid;
     
     public Article(int id, String title, String content, String regDate) {
         this.id = id;
@@ -12,4 +13,12 @@ public class Article {
         this.content = content;
         this.regDate = regDate;
     }
+    
+    @Override
+	public String toString() { //toString 메소드 오버라이딩
+		return id + ":" + title + ":" + content + ":" + regDate;
+	}
+    
+    
+    
 }
