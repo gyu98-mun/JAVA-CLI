3ffac0de7c1434232917db511a6a0a2305589e03
gyu98-mun <ansrb537@gmail.com>
2026-02-13 15:08:40 +0900

    main Loop 및 명령어 처리, 프로그램 종료 구현

--- JavaCLI/App.java
+++ JavaCLI/App.java
@@ -1,5 +1,77 @@
 package JavaCLI;
 
-public class App {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+// 메인 로직 클래스
+public class App {
+	// ArrayList 정의
+	ArrayList<Article> articles = new ArrayList<>();
+	// 사용자 명령어 받는곳 정의
+	Scanner sc = new Scanner ( System.in );
+	
+	// 프로그램 시작
+	public void run() {
+		System.out.println("==게시판 프로그램 시작==");
+		
+		// 명령어 받는 Loop
+		while(true) {
+
+			// 사용자 명령어 받기
+			System.out.print("명령어를 입력하세요 : ");
+			String input = sc.nextLine();
+			
+	        Rq rq = new Rq(input);
+	        String cmd = rq.getCmd();
+			
+			// 명령어 받아 처리 하는 곳
+			if (cmd.equals("exit")) {
+				break;
+			} else if (cmd.equals("write")) {
+				write();
+	        } else if (cmd.equals("list")) {
+	        	list();
+	        } else if (cmd.equals("detail")) {
+	        	detail(rq.getIntParam(0));
+	        } else if (cmd.equals("update")) {
+	        	update(rq.getIntParam(0));
+	        } else if (cmd.equals("delete")) {
+	        	delete(rq.getIntParam(0));
+	        } else { // 없는 명령어 입력시 답변
+	            System.out.println("존재하지 않는 명령어입니다.");
+	        }
+			
+		}
+	}
+	
+	// 진짜 메소드들 구현은 여기에
+	void write() {
+	 // TODO: A
+	}
+
+	void list() {
+	 // TODO: A
+	}
+
+	void detail(int id) {
+	 // TODO: B
+	}
+
+	void update(int id) {
+	 // TODO: C
+	}
+
+	void delete(int id) {
+	 // TODO: D
+	}
+
+	// Article 에 저장된 것을 id로 찾아오는 함수 구현
+	Article findById(int id) {
+	    for(Article a : articles) {
+	        if(a.getId() == id) return a; 
+	    }
+	    return null;
+	}
 }
+
+
--- JavaCLI/TEST/ArticleTest_1.java
+++ JavaCLI/TEST/ArticleTest_1.java
@@ -1,26 +1,28 @@
 package JavaCLI.TEST;
 
-import JavaCLI.Article ;
-import java.util.ArrayList;
+import JavaCLI.Article;
 import java.util.List;
+import java.util.ArrayList;
 
-// Article 생성 Test
-public class ArticleTest_1{
-	public static void main (String[] args) {
-		// 데모 DATA 선언
-		Article article_1 = new Article(1, "가", "가가", "2026-02-12");
-		Article article_2 = new Article(2, "나", "나나", "2026-02-13");
-		Article article_3 = new Article(3, "다", "다다", "2026-02-14");
-		Article article_4 = new Article(4, "라", "라라", "2026-02-15");
-		
-		// article 리스트 생성 및 데모 DATA 입력
-		List<Article> articleList = new ArrayList<>();
-		articleList.add(article_1);
-		articleList.add(article_2);
-		articleList.add(article_3);
-		articleList.add(article_4);
-		
-		System.out.println(articleList);
-		
-	}
+public class ArticleTest_1 {
+    public static void main(String[] args) {
+        // 데모 DATA 선언
+        Article article_1 = new Article(1, "가", "가가");
+        Article article_2 = new Article(2, "나", "나나");
+        Article article_3 = new Article(3, "다", "다다");
+        Article article_4 = new Article(4, "라", "라라");
+        
+        // article 리스트 생성 및 데모 DATA 입력
+        List<Article> articleList = new ArrayList<>();
+        articleList.add(article_1);
+        articleList.add(article_2);
+        articleList.add(article_3);
+        articleList.add(article_4);
+        
+        // DATA 출력 테스트
+        System.out.println("====== DATA 출력 테스트 ======");
+        for(Article a : articleList) {
+            System.out.println(a);
+        }
+    }
 }
